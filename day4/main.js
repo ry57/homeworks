4b54f7bd2db00d176ae89782d0ac52b1b36f6547
rayyan <rayyan-hrb@hotmail.com>
2023-08-10 02:27:15 +0300

    واجب اليوم الرابع

---
+++ day4/main.js
@@ -0,0 +1,52 @@
+// بداية التمرين الاول 
+
+const num_1 = 42;
+const NumToStr = String(num_1);
+console.log(`${typeof num_1} : ${num_1}`);
+console.log(`${typeof NumToStr} : ${NumToStr}`);
+
+const str_1 = "25";
+const StrToNum = Number(str_1);
+console.log(`${typeof str_1} : ${str_1}`);
+console.log(`${typeof StrToNum} : ${StrToNum}`);
+
+// نهاية التمرين الاول 
+// بداية التمرين الثاني
+
+const str_2 = "10" - "10";
+const str_3 = "10" + "10";
+console.log(`${str_2} , ${str_3}`);
+
+// نهاية التمرين الثاني
+
+// بداية التمرين الثالث
+
+const str_4 = "15.5";
+const StrToFloat = Number(str_4);
+console.log(`${typeof str_4} : ${str_4}`);
+console.log(`${typeof StrToFloat} : ${StrToFloat}`);
+
+const $a = StrToFloat * StrToFloat;
+console.log($a);
+const $b = StrToFloat / StrToFloat;
+console.log($b);
+
+// نهاية التمرين الثالث
+
+// بداية التمرين الرابع
+
+const str_5 = "5";
+const num_2 = 3;
+const plus = str_5 + num_2;
+console.log(plus);
+
+// نهاية التمرين الرابع
+
+// بداية التمرين الخامس
+
+const bool = true;
+const num_3 = 1;
+const BoolPlusNum = bool + num_3;
+console.log(BoolPlusNum);
+
+// نهاية التمرين الخامس 
