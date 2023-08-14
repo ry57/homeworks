e36310573f31e92ee4720875d86c8a457248f508
rayyan <rayyan-hrb@hotmail.com>
2023-08-14 15:01:17 +0300

    واجب يوم الاحد

---
+++ day6/main.js
@@ -0,0 +1,10 @@
+let names = ["ريان","علي","سماح","زياد","ابراهيم"];
+console.log(names[2]);
+console.log(names.unshift("سعد"));
+console.log(names);
+console.log(names.push("بتال"));
+console.log(names);
+console.log(names.shift());
+console.log(names);
+console.log(names.pop());
+console.log(names);
