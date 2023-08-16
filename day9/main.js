2ff873a47f4e4838267f95ccfb14034f12cc8760
rayyan <rayyan-hrb@hotmail.com>
2023-08-16 23:13:18 +0300

    واجب يوم الاربعاء

---
+++ day9/main.js
@@ -0,0 +1,29 @@
+// 1
+for (let a = 1; a <= 30; a++) {
+    if (a ==10) {
+        break;
+    }
+    console.log(a);
+}
+
+// 2
+for (let b = 1; b <= 20; b++) {
+    if (b==10) {
+        b+=1;
+    }
+    console.log(b);
+}
+
+// 3
+let c = 3;
+while (c>=1) {
+    console.log(c);
+    c--
+}
+
+// 4
+let d = 0;
+do {
+    console.log(d);
+    d++
+} while (d<=5);
