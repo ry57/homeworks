6d4fd5fee50feb849f569555b5d8b25ad586acf5
rayyan <rayyan-hrb@hotmail.com>
2023-08-21 13:32:17 +0300

    واجب يوم الاحد

---
+++ day11/index.js
@@ -0,0 +1,15 @@
+// 1
+function javascript_Boot_camp (teacher1,teacher2) {
+    alert(`${"This javaScript BootCamp with "}${teacher1}${" ok?"}`)
+    alert(`${"This javaScript BootCamp with "}${teacher2}${" ok?"}`)
+}
+javascript_Boot_camp("teacher1","teacher2")
+
+
+
+// 2
+function average(num_1,num_2,num_3,num_4) {
+    alert((num_1+num_2)/2)
+    alert((num_3+num_4)/2)
+}
+average(10,2,5.5,2.2)
